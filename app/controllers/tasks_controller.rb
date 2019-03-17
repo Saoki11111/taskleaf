57c426b3b9686c18c656116ca6e95fba9aa5bc97
Saoki11111 <t.aoki11111@gmail.com>
2019-03-17 18:24:30 +0900

    [update] add edit action and update actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
   def show
     @task = Task.find(params[:id])
   end
@@ -11,6 +12,13 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice:"タスク「#{task.name}」を更新しました。"
   end
 
   def create
